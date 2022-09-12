681439d843c83ed7b47af9ff2dd53323ecfe1453
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-12 17:06:43 +0200

    feat: import Font Awesome icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEarthEurope } from '@fortawesome/free-solid-svg-icons';
+
+const element = <FontAwesomeIcon icon={faEarthEurope} />
 
 function App() {
   return (
     <div className="App">
-      
+      {element}
     </div>
   );
 }
