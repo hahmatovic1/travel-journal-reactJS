2b106515d5730f7cc9394ead3cbff2863928a7d8
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-12 23:14:54 +0200

    feat: Add Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,19 +6,22 @@ const element = <FontAwesomeIcon icon={faLocationDot} />
 
 
 export default function Card(){
-    return <div class='card'>
-        <div className='card-img'>
-            <img src="http://source.unsplash.com/sblp4evk2gs" className="card--image" alt="mountains"/>
+    return <>
+    <div class='card no-margin'>
+        <div  className='card-img'>
+            <img src="http://source.unsplash.com/sblp4evk2gs" class="img" alt="mountains"/>
         </div>
         <div className='card-text'>
-            <div>
-                <h2>{element} Japan</h2> 
+            <div className='card-location'>
+                <p>{element} JAPAN</p> 
                 <p><a href="www.google.com">View on Google Maps</a></p>
-                <h1>Mount Fuji</h1>
-                <h4>12.jan - 12.jan</h4>
-                <p>Mostar is a city in southern Bosnia and Herzegovina, straddling the Neretva River. It’s known for the iconic Stari Most (Old Bridge), a reconstructed medieval arched bridge</p>
-            </div>
+            </div>    
+            <h1>Mount Fuji</h1>
+            <h4>12.jan - 12.jan</h4>
+            <p class='card-descriptio'>Mostar is a city in southern Bosnia and Herzegovina, straddling the Neretva River. It’s known for the iconic Stari Most (Old Bridge), a reconstructed medieval arched bridge</p>
             
         </div>
-    </div>
+        </div>
+    <hr class="card-line"/>
+    </>
 }
