874ef9a50e69b8ac0377791ce1b09b4494ff6a75
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-12 18:08:17 +0200

    feat: Add data.js as journal entries

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        title: "Mountain lodge Kraljevac",
+        location: "Bosnia and Herzegovina",
+        googleMapsUrl: "https://goo.gl/maps/bfFPXSpjdJTjX4SZ6",
+        startDate: "9 Sept, 2022",
+        endDate: "9 Sept, 2022",
+        description: "Mountain lodge 'Ibrahim Fejzić - Deda' Kraljevac is located at an altitude of 965 meters, at the foot of the northern slopes of Hranisava peak. The home is owned by the Pazarić Mountaineering and Skiing Association",
+        imageUrl: "https://unsplash.com/photos/sblp4evk2gs"
+    },
+    {
+        title: "Sarajevo",
+        location: "Bosnia and Herzegovina",
+        googleMapsUrl: "https://goo.gl/maps/ZgsU4Q5HiGwZEQR47",
+        startDate: "8 August, 2022",
+        endDate: "10 August, 2022",
+        description: "Sarajevo, capital of Bosnia and Herzegovina, is a compact city on the Miljacka River, surrounded by the Dinaric Alps. Its center has museums commemorating local history, including Sarajevo 1878–1918, which covers the 1914 assassination of Archduke Franz Ferdinand, an event that sparked World War I",
+        imageUrl: "https://unsplash.com/photos/XqSkdJBzxt0"
+    },
+    {
+        title: "Mostar",
+        location: "Bosnia and Herzegovina",
+        googleMapsUrl: "https://goo.gl/maps/gBx9bcKCDp4EpfyW9",
+        startDate: "25 July, 2022",
+        endDate: "27 July, 2022",
+        description: "Mostar is a city in southern Bosnia and Herzegovina, straddling the Neretva River. It’s known for the iconic Stari Most (Old Bridge), a reconstructed medieval arched bridge",
+        imageUrl: "https://unsplash.com/photos/_5pswmdUFqo"
+    }   
+]
+
+export default data;
