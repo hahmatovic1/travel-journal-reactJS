f392ecde78a500700cabcd6b727f64915c3f7e30
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-12 18:58:44 +0200

    feat: Add component JournalEntry

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import JournalEntry from './components/JournalEntry';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <JournalEntry />
     </div>
   );
 }
---
+++ src/components/JournalEntry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function JournalEntry(){
+    return <div>
+        {/* display cards */}
+    </div>
+}
