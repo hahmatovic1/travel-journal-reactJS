dd9d05df3b49571b382ccec7898bd28106d1b6ad
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-12 23:59:34 +0200

    feat: Render data from Card props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,21 +5,21 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 const element = <FontAwesomeIcon icon={faLocationDot} />
 
 
-export default function Card(){
+export default function Card(props){
+    console.log("pass")
     return <>
     <div class='card no-margin'>
         <div  className='card-img'>
-            <img src="http://source.unsplash.com/sblp4evk2gs" class="img" alt="mountains"/>
+            <img src={props.imageUrl} class="img" alt="mountains"/>
         </div>
         <div className='card-text'>
             <div className='card-location'>
-                <p>{element} JAPAN</p> 
-                <p><a href="www.google.com">View on Google Maps</a></p>
+                <p>{element} {props.location}</p> 
+                <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             </div>    
-            <h1>Mount Fuji</h1>
-            <h4>12.jan - 12.jan</h4>
-            <p class='card-descriptio'>Mostar is a city in southern Bosnia and Herzegovina, straddling the Neretva River. It’s known for the iconic Stari Most (Old Bridge), a reconstructed medieval arched bridge</p>
-            
+            <h1>{props.title}</h1>
+            <h4>{props.startDate} - {props.endDate}</h4>
+            <p class='card-description'>{props.description}</p>
         </div>
         </div>
     <hr class="card-line"/>
--- src/components/JournalEntry.jsx
+++ src/components/JournalEntry.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import data from '../data';
 import Card from './Card';
 
 export default function JournalEntry(){
+    const cardsData = data.map(item => {
+        return ( <Card 
+            key = {item.id}
+            {...item}
+        />
+        )
+        })  
+
+
     return <div>
-        <Card />
+        {cardsData}
     </div>
 }
